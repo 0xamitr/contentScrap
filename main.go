a427f4629732bde3ed6dc4cdb203040aeb7d3726
0xamitr <0xamitr@gmail.com>
2025-07-03 08:40:10 +0000

    server

--- main.go
+++ main.go
@@ -4,28 +4,27 @@ import (
 	"contestScrap/scrap"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 
 func setupRouter() *gin.Engine {
-
 	r := gin.Default()
-
-	
+	r.Use(cors.Default())
 	return r
 }
 
 func main() {
 	r := setupRouter()
-	contests, err := scrap.GetContests()
-	for _, contest := range(contests){
-		fmt.Println(contest)
-	}
-	fmt.Println(err)
-	// for _, contest := range contests {
-	// 	fmt.Println(contest)
-	// }
-	// print(err)
-
-	r.Run(":8080")
+	r.GET("/", func(c *gin.Context){
+		contests, err := scrap.GetContests()
+		for _, contest := range(contests){
+			fmt.Println(contest)
+		}
+		if(err != nil){
+			c.JSON(400, err)
+		}
+		c.JSON(200, contests)
+	})
+	r.Run("0.0.0.0:8080")
 }
