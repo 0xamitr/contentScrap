6959e2039e46dd6baa993974b5784d8664ddd27e
0xamitr <0xamitr@gmail.com>
2025-07-07 15:09:06 +0530

    fix: time format

--- scrap/codeforces/codeforces.go
+++ scrap/codeforces/codeforces.go
@@ -1,20 +1,20 @@
 package codeforces
 
 import (
-    "fmt"
-	"net/http"
-	"io/ioutil"
-	"time"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 type Contest struct {
-	ID                int    `json:"id"`
-	Name              string `json:"name"`
-	Type              string `json:"type"`
-	Phase             string `json:"phase"`
-	DurationSeconds   int64  `json:"durationSeconds"`
-	StartTimeSeconds  int64  `json:"startTimeSeconds"`
+	ID                 int    `json:"id"`
+	Name               string `json:"name"`
+	Type               string `json:"type"`
+	Phase              string `json:"phase"`
+	DurationSeconds    int64  `json:"durationSeconds"`
+	StartTimeSeconds   int64  `json:"startTimeSeconds"`
 	RelativeTimeSeconds int64 `json:"relativeTimeSeconds"`
 }
 
@@ -25,27 +25,43 @@ type ContestListResponse struct {
 
 func GetContests() ([]map[string]interface{}, error) {
 	res, err := http.Get("https://codeforces.com/api/contest.list")
-	
-	if(err != nil){
+	if err != nil {
 		fmt.Println(err)
+		return nil, err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
- 
-	var Response ContestListResponse
-	err = json.Unmarshal(body, &Response)
+	if err != nil {
+		return nil, err
+	}
+
+	var response ContestListResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	// Load IST timezone
+	ist, err := time.LoadLocation("Asia/Kolkata")
+	if err != nil {
+		// Fallback in case tzdata isn't available
+		ist = time.FixedZone("IST", 5*60*60+30*60)
+	}
 
 	var contests []map[string]interface{}
 
-	for _, contest := range Response.Result{
-		if(contest.Phase == "BEFORE"){
+	for _, contest := range response.Result {
+		if contest.Phase == "BEFORE" {
+			start := time.Unix(contest.StartTimeSeconds, 0).In(ist)
+
 			contests = append(contests, map[string]interface{}{
 				"title":    contest.Name,
-				"start":    time.Unix(contest.StartTimeSeconds, 0).Format("2006-01-02 15:04"),
+				"start":    start.Format("2006-01-02T15:04:05-07:00"), // ISO8601 with offset
 				"duration": contest.DurationSeconds / 60,
 			})
 		}
 	}
-		return contests, nil
+
+	return contests, nil
 }
